e6aa99533d1fe87e854e85572114202e3a0c5703
shishupal kumar <shishupalkumar0926@gmail.com>
2026-04-06 21:00:14 +0530

    fix allowed hosts

--- tips_prediction/settings.py
+++ tips_prediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%pc8hu2-4)3+h^%^j!hkhr4+xz4%re*78+6_5tn(n)*a$j%+%m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
